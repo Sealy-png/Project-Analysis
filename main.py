7fed45fdc39c5b364831259b3f9016a317df6917
ge63ciq <ammar.akkaya@mytum.de>
2024-12-04 16:00:35 +0100

    testpush comment jakob

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import decimal
 import numpy as np
 import tkinter as tk
 
-
+# TESTLINE JAKOB GIT
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
